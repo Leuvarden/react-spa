e9b6ed1911c432837742147671328c53d4353e1d
Maryna Leanovich <sollferino@gmail.com>
2018-04-20 20:30:07 +0300

    react-spa: edited webpack.config, adedd sass instead of css

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,7 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const StyleLintPlugin = require('stylelint-webpack-plugin');
 
 module.exports = {
     entry: './src/index.js',
@@ -7,24 +9,35 @@ module.exports = {
         path: path.join(__dirname, '/dist'),
         filename: 'index.bundle.js'
     },
+    devtool: 'source-map',
+    resolve: {
+        extensions: ['.js']
+    },
     module: {
         rules: [
             {
                 test: /\.js$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: 'babel-loader'
-                },
+                loader: 'babel-loader',
+                exclude: /node_modules/
             },
             {
-                test: /\.css$/,
-                use: ['style-loader', 'css-loader']
+                test: /\.(s*)css$/,
+                // use: ['style-loader', 'css-loader'] 
+                use: ExtractTextPlugin.extract({
+                    fallback: 'style-loader',
+                    use: 'css-loader!sass-loader'
+                })
             }
         ]
     },
     plugins: [
         new HtmlWebpackPlugin({
             template: './src/index.html'
+        }),
+        new ExtractTextPlugin('[name].css'),
+        new StyleLintPlugin({
+            configFile: '.stylelintrc',
+            failOnError: false
         })
     ]
 };
