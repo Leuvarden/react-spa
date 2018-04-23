f6b1896d8da3c2b63e2f6906bf34d76f02fb1ab3
Maryna Leanovich <sollferino@gmail.com>
2018-04-23 11:05:00 +0300

    react-spa: modified webpack.config

--- webpack.config.js
+++ webpack.config.js
@@ -3,41 +3,48 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const StyleLintPlugin = require('stylelint-webpack-plugin');
 
-module.exports = {
-    entry: './src/index.js',
-    output: {
-        path: path.join(__dirname, '/dist'),
-        filename: 'index.bundle.js'
-    },
-    devtool: 'source-map',
-    resolve: {
-        extensions: ['.js']
-    },
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                loader: 'babel-loader',
-                exclude: /node_modules/
-            },
-            {
-                test: /\.(s*)css$/,
-                // use: ['style-loader', 'css-loader'] 
-                use: ExtractTextPlugin.extract({
-                    fallback: 'style-loader',
-                    use: 'css-loader!sass-loader'
-                })
-            }
+module.exports = function (env, options) {
+
+    const isProduction = (options.mode === 'production');
+
+    const config = {
+        entry: './src/index.js',
+        output: {
+            path: path.join(__dirname, '/dist'),
+            filename: 'index.bundle.js'
+        },
+        devtool: isProduction ? 'none' : 'source-map',
+        resolve: {
+            extensions: ['.js']
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.js$/,
+                    loader: 'babel-loader',
+                    exclude: /node_modules/
+                },
+                {
+                    test: /\.(s*)css$/,
+                    // use: ['style-loader', 'css-loader'] 
+                    use: ExtractTextPlugin.extract({
+                        fallback: 'style-loader',
+                        use: 'css-loader!sass-loader'
+                    })
+                }
+            ]
+        },
+        plugins: [
+            new HtmlWebpackPlugin({
+                template: './src/index.html'
+            }),
+            new ExtractTextPlugin('[name].css'),
+            new StyleLintPlugin({
+                configFile: '.stylelintrc',
+                failOnError: false
+            })
         ]
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: './src/index.html'
-        }),
-        new ExtractTextPlugin('[name].css'),
-        new StyleLintPlugin({
-            configFile: '.stylelintrc',
-            failOnError: false
-        })
-    ]
+    };
+
+    return config;
 };
